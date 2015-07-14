a836f980fde9dec83bc37130fd71e79be0ad85f9
Luke O'Neill <Athena@Lukes-MacBook-Pro.local>
2015-07-14 15:50:25 -0400

    Fixed some position issues

--- app/src/main/java/com/kooknluke/skidice/MainActivity.java
+++ app/src/main/java/com/kooknluke/skidice/MainActivity.java
@@ -1,11 +1,13 @@
 package com.kooknluke.skidice;
 
+import android.graphics.PorterDuff;
 import android.support.v7.app.ActionBarActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,65 +19,36 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-    	actionBar.hide();
-        
+        actionBar.hide();
+
         final Context context = this;
-        
+
 //        Button btnJump = (Button) findViewById(R.id.btnJumps);
 //        Button btnRail = (Button) findViewById(R.id.btnRails);
 //        Button btnSlopeStyle = (Button) findViewById(R.id.btnSlope);
-        Button btnApres = (Button) findViewById(R.id.btnApres);
-        Button btnShakeJump = (Button) findViewById(R.id.btnJumps);
-        Button btnDiffSel = (Button) findViewById(R.id.btnDifficulty);
-        Button btnShakeRail = (Button) findViewById(R.id.btnRails);
-        Button btnSlope = (Button) findViewById(R.id.btnSlopeStyle);
-        
-        
-//        btnJump.setOnClickListener(new View.OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				// open jump activity when jump is selected
-//				Intent intent = new Intent(context, Jump.class);
-//				startActivity(intent);
-//			}
-//		});
-//
-//        btnRail.setOnClickListener(new View.OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				// open rail activity when rail is selected
-//				Intent intentRail = new Intent(context, Rail.class);
-//				startActivity(intentRail);
-//			}
-//		});
-//
-//        btnSlopeStyle.setOnClickListener(new View.OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				// open slope activity when clicked
-//				Intent intentSlope = new Intent(context, SlopeSelect.class);
-//				startActivity(intentSlope);
-//			}
-//		});
-        
+        final Button btnApres = (Button) findViewById(R.id.btnApres);
+        final Button btnShakeJump = (Button) findViewById(R.id.btnJumps);
+        final Button btnDiffSel = (Button) findViewById(R.id.btnDifficulty);
+        final Button btnShakeRail = (Button) findViewById(R.id.btnRails);
+        final Button btnSlope = (Button) findViewById(R.id.btnSlopeStyle);
+
         btnApres.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// open slope activity when clicked
-				Intent intentSlope = new Intent(context, Apres.class);
-				startActivity(intentSlope);
-			}
-		});
+
+            @Override
+            public void onClick(View v) {
+                // open slope activity when clicked
+                buttonEffect(btnApres);
+                Intent intentSlope = new Intent(context, Apres.class);
+                startActivity(intentSlope);
+            }
+        });
 
         btnShakeJump.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // open slope activity when clicked
+                buttonEffect(btnShakeJump);
                 Intent intentSlope = new Intent(context, jumpShake.class);
                 startActivity(intentSlope);
             }
@@ -86,6 +59,7 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 // open slope activity when clicked
+                buttonEffect(btnDiffSel);
                 Intent intentSlope = new Intent(context, Difficulty_Selection.class);
                 startActivity(intentSlope);
             }
@@ -96,6 +70,7 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 // open slope activity when clicked
+                buttonEffect(btnShakeRail);
                 Intent intentSlope = new Intent(context, railShake.class);
                 startActivity(intentSlope);
             }
@@ -106,15 +81,34 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 // open slope activity when clicked
+                buttonEffect(btnSlope);
                 Intent intentSlope = new Intent(context, shakeSlope.class);
                 startActivity(intentSlope);
             }
         });
-        
-    }
-    
-    	
 
+    }
+
+    public static void buttonEffect(View btn){
+        btn.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        v.getBackground().setColorFilter(0xeee0000, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
